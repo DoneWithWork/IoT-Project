9ea866edfa81a65130064c7277431492a199ea00
DoneWithWork <donewithworkyt@gmail.com>
2025-07-03 18:38:54 +0800

    bug: Added env

--- apps/frontend/src/lib/env/server.ts
+++ apps/frontend/src/lib/env/server.ts
@@ -6,7 +6,8 @@ export const server = createEnv({
         NODE_ENV: z.string().min(1),
         CLERK_SECRET_KEY: z.string().min(1),
         CLERK_WEBHOOK_SIGNING_SECRET: z.string().min(1),
-        ENCRYPTION_KEY: z.string().min(1)
+        ENCRYPTION_KEY: z.string().min(1),
+        DATABASE_URL: z.string().url(),
     },
 
     experimental__runtimeEnv: process.env
